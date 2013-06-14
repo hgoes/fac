0e1d06c59f5cdea6ceefa0bf292cd594af3efd04
Henning Günther <h.guenther@tu-bs.de>
2013-06-14 08:54:58 +0200

    Make aiger parser more robust

--- Aiger.hs
+++ Aiger.hs
@@ -1,9 +1,9 @@
 module Aiger where
 
 import Literal
-
-readLit :: String -> Lit
-readLit = Lit . read
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.List as List
 
 data Aiger lit = Aiger { aigerMaxVar :: lit
                        , aigerInputs :: [lit]
@@ -17,22 +17,24 @@ data Aiger lit = Aiger { aigerMaxVar :: lit
 data Symbol = Input
             | Latch
             | Output
+            | Unknown
             deriving (Show,Eq,Ord)
 
-readAiger :: (String -> lit) -> String -> Aiger lit
-readAiger parse str = case lines str of
+readAiger :: Read lit => String -> Aiger lit
+readAiger str = case lines str of
   header:rest -> case words header of
-    ["aag",max_var,n_inp,n_latch,n_outp,n_and]
+    ("aag":max_var:n_inp:n_latch:n_outp:n_and:extras)
       -> let (inp_lines,rest1) = splitAt (read n_inp) rest
              (latch_lines,rest2) = splitAt (read n_latch) rest1
              (outp_lines,rest3) = splitAt (read n_outp) rest2
              (and_lines,rest4) = splitAt (read n_and) rest3
-             (syms,comms) = parseSymbols rest3
-         in Aiger { aigerMaxVar = parse max_var
-                  , aigerInputs = [ parse ln | ln <- inp_lines ]
-                  , aigerLatches = [ (parse l1,parse l2) | [l1,l2] <- fmap words latch_lines ]
-                  , aigerOutputs = [ parse ln | ln <- outp_lines ]
-                  , aigerGates = [ (parse l1,parse l2,parse l3) | [l1,l2,l3] <- fmap words and_lines ]
+             rest5 = drop (sum $ fmap read extras) rest4
+             (syms,comms) = parseSymbols rest5
+         in Aiger { aigerMaxVar = read max_var
+                  , aigerInputs = [ read ln | ln <- inp_lines ]
+                  , aigerLatches = [ (read l1,read l2) | [l1,l2] <- fmap words latch_lines ]
+                  , aigerOutputs = [ read ln | ln <- outp_lines ]
+                  , aigerGates = [ (read l1,read l2,read l3) | [l1,l2,l3] <- fmap words and_lines ]
                   , aigerSymbols = syms
                   , aigerComments = comms
                   }
@@ -48,5 +50,26 @@ readAiger parse str = case lines str of
                         'i' -> Input
                         'l' -> Latch
                         'o' -> Output
+                        _ -> Unknown
                       (syms,comms) = parseSymbols xs
                   in ((sym',read num,name):syms,comms)
+
+countUses :: (Literal lit) => Aiger lit -> Map Var Int
+countUses aiger = let inc key = Map.alter (\entr -> case entr of
+                                              Nothing -> Just 1
+                                              Just n -> Just (n+1)
+                                          ) key
+                      mp1 = foldl (\cmp (_,latch_from) -> inc (litVar latch_from) cmp) Map.empty (aigerLatches aiger)
+                      mp2 = foldl (\cmp outp -> inc (litVar outp) cmp) mp1 (aigerOutputs aiger)
+                      mp3 = foldl (\cmp (_,in1,in2) -> if litVar in1==litVar in2 
+                                                       then inc (litVar in1) cmp else inc (litVar in1) (inc (litVar in2) cmp)) mp2 (aigerGates aiger)
+                  in mp3
+
+isInput :: Literal lit => Var -> Aiger lit -> Bool
+isInput var aiger = case List.find (\lit -> litVar lit == var) (aigerInputs aiger) of
+  Nothing -> False
+  Just _ -> True
+
+getGate :: Literal lit => Var -> Aiger lit -> (lit,lit)
+getGate gate aiger = case List.find (\(gt,_,_) -> litVar gt == gate) (aigerGates aiger) of
+  Just (_,in1,in2) -> (in1,in2)
